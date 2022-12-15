b41eea3f5d7a34dab0e62c6c12f8ba5942be3f2a
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-15 13:32:24 +0700

    add example002. Hello user.(with ReadLine)

--- example002_hello_user/Program.cs
+++ example002_hello_user/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input your name: ");
+string username = Console.ReadLine();
+Console.Write("Hello,");
+Console.WriteLine(username);
