cd2d9ed554206201cb2c5d5ac91336ec2a4247c8
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-15 14:57:44 +0700

    add example4 with rand numbers

--- example004_devision/Program.cs
+++ example004_devision/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 14;
-double numberB = 5;
+﻿double numberA = new Random().Next (1,10); // rand in 1,2,3....9
+Console.WriteLine(numberA);
+double numberB = new Random().Next (1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
