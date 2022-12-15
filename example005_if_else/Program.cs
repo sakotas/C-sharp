f3d6dda89197e7817bc3096dc975c87c4e81fc7d
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-15 15:19:00 +0700

    add example005: if else conditions

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("input your name: ");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "vasya")
+{
+    Console.Write ("Hello, my dear ", name);
+    Console.WriteLine(name);
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(name);
+}
