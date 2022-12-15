488f01e3a6b3febc368abbc206657ca3d9655e53
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-16 00:58:40 +0700

    First task in sem1 added. Modified

--- Seminar1/sem1_3/Program.cs
+++ Seminar1/sem1_3/Program.cs
@@ -4,16 +4,49 @@
 //5 ->  Пятница
 
 using static System.Console;
-Write ("Input day of the week: ");
+Write("Input day of the week: ");
 int day = int.Parse(ReadLine()!);
 
-switch (day)
+/*switch (day)
 {
-    case 1: WriteLine("Monday"); break;
-    case 2: WriteLine("Tuesday"); break;
+    case 1: WriteLine("Monday"); break;case 2: WriteLine("Tuesday"); break;
     case 3: WriteLine("Wednesday"); break;
     case 4: WriteLine("Thursday"); break;
     case 5: WriteLine("Friday"); break;
     case 6: WriteLine("Saturday"); break;
     case 7: WriteLine("Sunday"); break;
+}
+
+*/
+if (day == 1)
+{
+    WriteLine("Monday");
+}
+if (day == 2)
+{
+    WriteLine("Tuesday");
+}
+if (day == 3)
+{
+    WriteLine("Wednesday");
+}
+if (day == 4)
+{
+    WriteLine("Thursday");
+}
+if (day == 5)
+{
+    WriteLine("Friday");
+}
+if (day == 6)
+{
+    WriteLine("Saturday");
+}
+if (day == 7)
+{
+    WriteLine("Sunday");
+}
+else
+{
+    WriteLine("I don't Know the day!!!");
 }
