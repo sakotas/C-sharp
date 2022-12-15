fd3d53bca63ab68e6fd329bd9704385864150112
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-16 00:53:24 +0700

    Add Second and 3rd tasks in sem1

---
+++ Seminar1/sem1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//1. Напишите программу, которая на вход принимает два числа и проверяет,
+//является ли первое число квадратом второго.
+//
+//a = 25; b = 5 -> да
+//a = 2 b = 10 -> нет
+//a = 9; b = -3 -> да
+//a = -3 b = 9 -> нет
+
+using static System.Console;
+Write ("Input first number: ");
+int a = int.Parse(ReadLine()!);
+Write ("Input second number: ");
+int b = int.Parse(ReadLine()!);
+
+if ((b * b) == a)
+    {
+        WriteLine("YES");
+    }
+else
+{
+    WriteLine("NO");
+}
+
---
+++ Seminar1/sem1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//
+//3 -> Среда
+//5 ->  Пятница
+
+using static System.Console;
+Write ("Input day of the week: ");
+int day = int.Parse(ReadLine()!);
+
+switch (day)
+{
+    case 1: WriteLine("Monday"); break;
+    case 2: WriteLine("Tuesday"); break;
+    case 3: WriteLine("Wednesday"); break;
+    case 4: WriteLine("Thursday"); break;
+    case 5: WriteLine("Friday"); break;
+    case 6: WriteLine("Saturday"); break;
+    case 7: WriteLine("Sunday"); break;
+}
