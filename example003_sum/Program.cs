1ae55d1a646c29445b85e987f1969f31f682eef1
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-15 14:54:57 +0700

    add random nubers to example 003 (sum)

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿double numberA = new Random().Next (1,10); // rand in 1,2,3....9
+Console.WriteLine(numberA);
+double numberB = new Random().Next (1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
