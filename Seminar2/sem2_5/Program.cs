5b5b61dfd37e4ea5edaf195f15ad327d7dcc65bd
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:38:48 +0300

    New input data for solution recieved. Code adjusted accordingly

--- Seminar2/sem2_5/Program.cs
+++ Seminar2/sem2_5/Program.cs
@@ -11,14 +11,18 @@ int num1 = int.Parse(ReadLine()!);
 WriteLine("Input number 2: ");
 int num2 = int.Parse(ReadLine()!);
 
-// if ((num1 * num1 == num2) || (num2 * num2 == num1))
+// if (num1 * num1 == num2)
 // {
-//     WriteLine("YES");
+//     WriteLine($"Второе число '{num2}' является квадратом первого числа '{num1}'");
+// }
+// else if (num2 * num2 == num1)
+// {
+//     WriteLine($"Первое число '{num1}' является квадратом второго числа '{num2}'");
+
 // }
 // else
 // {
-//     WriteLine("No");
-
+//     WriteLine("Ни одно число не является квадратом другого");
 // }
 
-WriteLine((num1 * num1 == num2) || (num2 * num2 == num1)?"Yes":"No");
+WriteLine(num1 * num1 == num2?$"Второе число '{num2}' является квадратом первого числа '{num1}'":num2 * num2 == num1?$"Первое число '{num1}' является квадратом второго числа '{num2}'":"Ни одно число не является квадратом другого");
