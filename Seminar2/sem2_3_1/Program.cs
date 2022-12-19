434088c16d8300520b09158a1b63e21e5b4cb471
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:10:13 +0300

    sem2_3_1 task updated and now seems cooler

--- Seminar2/sem2_3_1/Program.cs
+++ Seminar2/sem2_3_1/Program.cs
@@ -6,4 +6,17 @@ int num1 = int.Parse(ReadLine()!);
 WriteLine("Input number 2: ");
 int num2 = int.Parse(ReadLine()!);
 
-WriteLine(num1<num2?"<":">");
+// if (num1 < num2)
+// {
+//     WriteLine("<");
+// }
+// else if (num1 > num2)
+// {
+//     WriteLine(">");
+// }
+// else
+// {
+//     WriteLine("=");
+// }
+
+WriteLine(num1<num2?"<":num1>num2?">":"=");
