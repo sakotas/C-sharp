0ae3812d17c943891db0b277537c6fa0479742ef
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:00:48 +0300

    Additional task added and solved (because we molodcy and solved previous tasks very quickly

---
+++ Seminar2/sem2_3_1/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Input number 1: ");
+int num1 = int.Parse(ReadLine()!);
+
+WriteLine("Input number 2: ");
+int num2 = int.Parse(ReadLine()!);
+
+WriteLine(num1<num2?"<":">");
