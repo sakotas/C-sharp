6981169f1a8a6b9566bde0f5e922b0d5538dc44c
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:51:57 +0300

    sem2_6 added and solved

---
+++ Seminar2/sem2_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Два стрелка
+// На вход - сколько каждый стрелок сбил
+// На выход - общее количество банок
+
+using static System.Console;
+Clear();
+WriteLine("Банок у стрелка 1: ");
+int num1 = int.Parse(ReadLine()!);
+
+WriteLine("Банок у стрелка 2: ");
+int num2 = int.Parse(ReadLine()!);
+
+WriteLine($"Банок всего = {num1 + num2 - 1}");
