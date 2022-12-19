1ca1a157e41ad4e7c9a12d0a2e67fbe087c2d2b9
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 20:37:18 +0300

    Sem2_2 solved

--- Seminar2/sem2_1/Program.cs
+++ Seminar2/sem2_1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+// int number = new Random().Next(10,100);
+
+// int a1 = number/10;
+// int a2 = number%10;
+// int max = a1>a2?a1:a2;
+// WriteLine();
+WriteLine(new Random().Next(10,100)%2 == 0?"Even":"Odd");
+
+// if (a1>a2)
+// {
+//     WriteLine(a1);
+// }
+// else
+// {
+//     WriteLine(a2);
+// }
+
+// using static System.Console;
+// Clear();
+// int number=new Random().Next(10,100);
+// WriteLine(number);
+// WriteLine(number/10>number%10?number/10:number%10);
+// if(number/10>number%10)
+// {
+//     WriteLine(number/10);
+// }
+// else
+// {
+//     WriteLine(number%10);
+// }
---
+++ Seminar2/sem2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// **Задача 11:**
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+using static System.Console;
+Clear();
+
+int number = new Random().Next(100,1000);
+
+WriteLine(number);
+
+int a1 = number/100;
+int a2 = number%10;
+
+Write(a1);
+Write(a2);
