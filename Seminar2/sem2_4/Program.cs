ca9c6253b977fb2bce7ff714fcea4ba6763b3ba2
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:32:33 +0300

    Oneline solution added

--- Seminar2/sem2_4/Program.cs
+++ Seminar2/sem2_4/Program.cs
@@ -10,11 +10,12 @@ Clear();
 WriteLine("Input number: ");
 int num = int.Parse(ReadLine()!);
 
-if ((num % 7 == 0) && (num % 23 == 0))
-{
-    WriteLine("Да");
-}
-else
-{
-    WriteLine("Нет");
-}
+// if ((num % 7 == 0) && (num % 23 == 0))
+// {
+//     WriteLine("Да");
+// }
+// else
+// {
+//     WriteLine("Нет");
+// }
+WriteLine((num % 7 == 0) && (num % 23 == 0)?"Да":"Нет");
