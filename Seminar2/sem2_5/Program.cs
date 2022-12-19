4e4f526976cd093fe3fb57350b098e58bf73b6fa
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-19 21:31:19 +0300

    sem2_5 added and solved

---
+++ Seminar2/sem2_5/Program.cs
@@ -0,0 +1,24 @@
+﻿// **Задача 16:**
+
+// Напишите программу, которая принимает на вход два числа
+// и проверяет, является ли одно число квадратом другого.
+
+using static System.Console;
+Clear();
+WriteLine("Input number 1: ");
+int num1 = int.Parse(ReadLine()!);
+
+WriteLine("Input number 2: ");
+int num2 = int.Parse(ReadLine()!);
+
+// if ((num1 * num1 == num2) || (num2 * num2 == num1))
+// {
+//     WriteLine("YES");
+// }
+// else
+// {
+//     WriteLine("No");
+
+// }
+
+WriteLine((num1 * num1 == num2) || (num2 * num2 == num1)?"Yes":"No");
