669bf0dd90e23f19292e64d4b10585bf088f1f11
Maksim_Dl <maksimdolgov@mail.ru>
2022-12-23 02:09:28 +0700

    add tasks 4,5 for sem 3

---
+++ Seminar3/sem3_4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+using static System.Console;
+Clear ();
+
+Write("input x1:");
+if (!int.TryParse(ReadLine(), out int X1))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input y1:");
+if (!int.TryParse(ReadLine(), out int Y1))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input x2:");
+if (!int.TryParse(ReadLine(), out int X2))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input y2:");
+if (!int.TryParse(ReadLine(), out int Y2))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+//Math.Sqrt
+//Math.Pow(x, stepen))
+double Val = Math.Sqrt (Math.Pow((X1 - X2),2) +  Math.Pow((Y1 - Y2),2));
+WriteLine ("Distanse between 2 points is {0:f2}", Val);
+
---
+++ Seminar3/sem3_5/Program.cs
@@ -0,0 +1,32 @@
+﻿// Вывести квадраты чисел от 1 до N
+
+
+using static System.Console;
+Clear();
+
+Write("input N:");
+if (!int.TryParse(ReadLine(), out int N))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+if (N > 0)
+{
+    int index = 1;
+    while (index <= N)
+    {
+        Write($"{Math.Pow(index, 2)} ");
+        index += 1;
+    }
+    WriteLine(" ");
+}
+else
+{
+    int index = 1;
+    while (index >= N)
+    {
+        Write($"{Math.Pow(index, 2)} ");
+        index -= 1;
+    }
+    WriteLine(" ");
+}
