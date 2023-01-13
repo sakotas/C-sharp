9c16f230ca20d568116cb127afbf8645e70af56b
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-13 16:44:56 +0700

    add half of sem4 homework (checking git highliting)

---
+++ Sem4_Homework/sem4_hw1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// В задаче не использвать стандартный метод Pow!
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+using static System.Console;
+Clear();
+
+WriteLine("make number A to the B power (natural) (without using math library)");
+
+Write("input any integer number A:");
+if (!int.TryParse(ReadLine(), out int a))
+{
+    WriteLine("Error! Input number, not string or float");
+    return -1;
+}
+
+Write("input Natural number B:");
+if (!int.TryParse(ReadLine(), out int b))
+{
+    WriteLine("Error! Input number, not string or float");
+    return -1;
+}
+if (b <= 0) 
+{
+    WriteLine("Only natural number allowed!");
+    return -1;
+}
+
+
+WriteLine($"{a} to the power {b} = {PowAB(a,b)}");
+return 1;
+
+
+int PowAB (int a, int b)
+{
+    int result = 1;
+    for (int i=1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
---
+++ Sem4_Homework/sem4_hw2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+using static System.Console;
+
+
+WriteLine("Sum of all digits ");
+
+Write("input any integer number: ");
+if (!int.TryParse(ReadLine(), out int TheNumber))
+{
+    WriteLine("Error! Input int number, not string or float ");
+    return -1;
+}
+
+WriteLine($"sum of digits of the number {TheNumber} is {SumOfDigits(TheNumber)}");
+return 1;
+
+int SumOfDigits(int number)
+{
+    int result = 0;
+    number = Math.Abs(number);
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    
+    return result;
+}
