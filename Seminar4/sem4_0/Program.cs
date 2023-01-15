011cf3b92f419d10171152a92d5e3d34c8035271
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-01-15 19:47:58 +0300

    4_0 added - contains array generator methods (manual and random) + PrintArray Method

---
+++ Seminar4/sem4_0/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+Clear();
+
+int[] ar = new int[8];
+
+PrintArray(ar);
+PrintArray(FillArrayRandom(ar));
+PrintArray(FillArrayManual(ar));
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+
+int[] FillArrayRandom(int[] array)
+{
+    int[] result = new int[array.Length];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = new Random().Next(1, 99);
+    }
+
+    return result;
+}
+
+int[] FillArrayManual(int[] array)
+{
+    int[] result = new int[array.Length];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"Введите число в индекс массива [{i}]:");
+        result[i] = int.Parse(ReadLine()!);
+        WriteLine();
+    }
+
+    return result;
+}
