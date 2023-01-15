a0e44d7cec73d22b76b4b278b06da479494bed70
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-01-15 20:06:49 +0300

    sem4_4 solved. yeah!

---
+++ Seminar4/sem4_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] arr = new int[8];
+
+PrintArray(InitFillArrayRandom());
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+
+int[] InitFillArrayRandom()
+{
+    int[] result = new int[8];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+
+    return result;
+}
