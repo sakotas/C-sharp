1524e70f19b027b87ff5ac05212b6f27c78d4f6a
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-01-15 17:24:56 +0300

    sem4_3 solved

--- Seminar4/sem4_3/Program.cs
+++ Seminar4/sem4_3/Program.cs
@@ -4,3 +4,19 @@ Clear();
 // Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24
 // 5 -> 120
+
+Write("Input number n: ");
+int number = int.Parse(ReadLine()!);
+
+WriteLine($"Product of number 1..n = {GetProduct(number)}");
+
+int GetProduct(int n)
+{
+    int result = 1;
+    while (n > 0)
+    {
+        result = result * n;
+        n--;
+    }
+    return result;
+}
