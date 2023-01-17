5badcf7883b5e91185ae2e3519a6eb16a4072e6e
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-17 15:56:58 +0700

    add lecture1 folder anf files in it

---
+++ Lecture1/example003_sum/Program.cs
@@ -0,0 +1,5 @@
+﻿double numberA = new Random().Next (1,10); // rand in 1,2,3....9
+Console.WriteLine(numberA);
+double numberB = new Random().Next (1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
