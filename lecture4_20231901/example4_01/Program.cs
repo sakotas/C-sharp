4c4dfcbe1a198e49892daa2382cd02e96b064507
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-19 13:58:51 +0700

    lection 4 added (20220119)

---
+++ lecture4_20231901/example4_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+Clear();
+
+string[,] table = new string[2, 5];
+table[1, 2] = "word";
+
+
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         WriteLine($"-{table[rows,columns]}-");
+//     }
+
+// }
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+WriteLine();
+PrintArray(matrix);
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(0,10);
+        }
+    }
+}
---
+++ lecture4_20231901/example4_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+
+ for (int i =0; i < 25; i++)
+ {
+     WriteLine($"{i}! = {factorial(i)}");
+ }
+
+
+
+double factorial(int f)
+{   
+    if (f == 1 || f ==0) return 1;
+    else return f* factorial(f-1);
+    
+}
