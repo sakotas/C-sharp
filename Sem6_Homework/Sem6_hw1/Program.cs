0696216ad33d2008aeed2979df01b479aaa2016d
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-21 15:57:55 +0700

    add 2 tasks for sem6 homework

---
+++ Sem6_Homework/Sem6_hw1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+using static System.Console;
+Clear();
+
+WriteLine("count positive numbers ");
+WriteLine("Input several numbers(even text allowed if need), separated by space. Press enter after finished");
+bool ignory = true;  // if text is prophibited change ingnore to false!  (bool ignory = false;)
+string[] InputArray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//!!!!!!!!!!! if need input exactly M numbers(as in task above): comment 1 string above, uncomment block below!!!!!!
+
+/* 
+Write("Input how much numbers are to input (m)");
+if (!int.TryParse(ReadLine(), out int m))
+{
+    WriteLine("M Must be Positive int value!");
+    Environment.Exit(0);
+}
+if (m<=0)
+{
+    WriteLine("M Must be Positive int value!");
+    Environment.Exit(0);
+}
+string[] InputArray = new string[m];
+for (int i=0; i< m; i++)
+{
+    Write($"input {i+1} number: ");
+    InputArray[i] = ReadLine();
+}
+*/ 
+
+WriteLine($"Number of positive elements is: {CountPositives(InputArray, ignory)}");  // if add argument to CountPositives "ignore:false" - exit progr if char elements found (default true)
+
+///////////////////// methods below ///////////////////
+
+// count positive numbers in array
+// if ignore = true - count even if char elements found
+// if ignore = false - stop counting, exit programm
+int CountPositives(string[] InArr, bool ignore = true)
+{
+    //double[] ArrayDouble = new double[InArr.Length];
+    double temp = 0;
+    int count = 0;
+    for (int i = 0; i < InArr.Length; i++)
+    {
+        if (!double.TryParse((InArr[i]), out temp) & !ignore)
+        {
+            WriteLine("Not int or double found (char). Cant work farther. Now exit at all!!!");
+            Environment.Exit(0);
+        }
+        else
+        {
+            count += temp > 0 ? 1 : 0;
+        }
+    }
+
+    double[] ArrayDoubel = new double[InArr.Length];
+    return count;
+}
---
+++ Sem6_Homework/Sem6_hw2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+Clear();
+
+WriteLine("Find interseption of functions: y = k1 * x + b1, y = k2 * x + b2");
+
+double b1 = CheckInput("Input b1: ");
+double k1 = CheckInput("Input k1: ");
+double b2 = CheckInput("Input b2: ");
+double k2 = CheckInput("Input k2: ");
+
+if (k1 == k2 && b1 == b2)
+{
+    WriteLine("Infinitive number of solutions: lines are equal");
+}
+else if (k1 == k2)
+{
+    WriteLine("No solutions: Lines ara parallel ");
+}
+else
+{
+    double x = (b2-b1) / (k1 - k2);
+    double y = (k1 * (b2-b1))/ (k1-k2) + b1;
+    WriteLine($"There is one solution(interseption): x={x}, y={y}");
+}
+
+
+double CheckInput(string message)
+{
+    Write(message);
+    if (!double.TryParse(ReadLine(), out double result))
+    {
+        WriteLine("Only int and double values are allowed. Now terminate programm");
+        Environment.Exit(0);
+    }
+
+    return result;
+
+}
