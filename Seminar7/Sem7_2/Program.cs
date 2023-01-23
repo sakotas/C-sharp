a7ab9499e5e2015828e1c3e93e6b14e53b1234dd
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-24 01:01:57 +0700

    add tasks 1-3 for sem7

---
+++ Seminar7/Sem7_2/Program.cs
@@ -0,0 +1,44 @@
+﻿// **Задача 48:** Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+using static System.Console;
+Clear();
+Write("Input size of array (m,n) ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetNewArray(int.Parse(parameters[0]), int.Parse(parameters[1]));
+Print2dArray(array);
+
+int[,] GetNewArray(int m, int n)
+{
+    int[,] resultArray = new int[m,n];
+    for(int i = 0; i< m; i++)
+    {
+        for(int j=0; j< n; j++)
+        {
+            resultArray[i,j] = i+j;
+        }
+    
+    }
+    return resultArray;
+}
+
+void Print2dArray(int[,] inArray)
+{
+     for(int i = 0; i< inArray.GetLength(0); i++)
+    {
+        for(int j=0; j< inArray.GetLength(0); j++)
+        {
+            Write($" {inArray[i,j]}" );
+        }
+    WriteLine("");
+    }
+   
+}
---
+++ Seminar7/Sem7_3/Program.cs
@@ -0,0 +1,75 @@
+﻿// **Задача 49:** Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// кроме нулевого индекса!
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 7 2
+
+// 5 **81** 2 **9**
+
+// 8 4 2 4
+
+using static System.Console;
+Clear();
+
+Write("Input size of matrix and min max values separated by space: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetMatrixArray(int.Parse(parameters[0]),int.Parse(parameters[1]),int.Parse(parameters[2]),int.Parse(parameters[3]));
+
+PrintMatrixArrray (array);
+EvenElementsPow2(array);
+WriteLine("");
+WriteLine("");
+PrintMatrixArrray (array);
+
+void EvenElementsPow2(int[,] inArray)
+{
+    for(int i=2; i< inArray.GetLength(0); i +=2)   
+    {
+     for(int j=2; j< inArray.GetLength(1); j +=2)
+     {
+        inArray[i,j] *= inArray[i,j];
+     }      
+    }
+}
+
+int [,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] resultArray = new int [rows, columns];
+    Random rnd = new Random();
+    for(int i =0; i < rows; i++)
+    {
+        for(int j =0; j < columns; j++)
+        {
+            resultArray[i,j] = rnd.Next(minValue, maxValue+1);
+        }
+    }
+return resultArray;
+}
+
+
+
+void PrintMatrixArrray (int[,] inArray)
+{
+    for(int i =0; i < inArray.GetLength(0); i++)
+    {
+        for(int j =0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j],5 }");  // фиксированный размер вывода
+        }
+    WriteLine();
+    }
+}
+
+
+
+
