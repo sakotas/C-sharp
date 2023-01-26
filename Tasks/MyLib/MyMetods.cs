d279ee8c47414c54a1e9f9c1b45512516485e873
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-27 02:57:12 +0700

    add folder tasks -  first test of own libraries

---
+++ Tasks/MyLib/MyMetods.cs
@@ -0,0 +1,57 @@
+﻿namespace MyLib;
+using static System.Console;
+public class MyMetods
+{
+    ///<summary>
+    /// Metod1 - // generate random filled int array wit size = size
+    ///</summary>
+    public static int[] GetLineArray(int size)  
+    {
+        int[] result = new int[size];
+        for(int i =0; i< size; i++)
+        {
+            result[i] = new Random().Next();
+        }
+        return result;
+    }
+
+    ///<summary>
+    /// Metod2 // generate random(from 0 to maxValue) filled int array wit size = size
+    ///</summary>
+    public static int[] GetLineArray(int size, int maxValue)
+    {
+        int[] result = new int[size];
+        for(int i =0; i< size; i++)
+        {
+            result[i] = new Random().Next(maxValue+1);
+        }
+        return result;
+    }
+
+
+    ///<summary> 
+    /// Metod3 // generate random(from minValue to maxValue) filled int array with size = size, 
+    ///</summary>
+    public static int[] GetLineArray(int size, int minValue, int maxValue) 
+    {
+        int[] result = new int[size];
+        for(int i =0; i< size; i++)
+        {
+            result[i] = new Random().Next(minValue, maxValue+1);
+        }
+        return result;
+    }
+
+    
+    ///<summary> 
+    /// print usual array
+    ///</summary>
+    public static void PrintArray(int[] inArray)
+    {
+
+
+    }
+
+
+
+}
---
+++ Tasks/Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+using MyLib;
+
+int[] array=MyMetods.GetLineArray(10);
+WriteLine($"{string.Join(" ", array)}");
+
+array=MyMetods.GetLineArray(10,10);
+WriteLine($"{string.Join(" ", array)}");
+
+array=MyMetods.GetLineArray(10,5,10);
+WriteLine($"{string.Join(" ", array)}");
