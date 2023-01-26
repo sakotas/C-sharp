fb018ee336e9e5327476aa2cffec785676d85892
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-26 23:45:57 +0700

    add examples from lectrure 5 - work with files, dir

---
+++ Lecture5_20230126_work_with_files/example01/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+string path = "G:/GEEK/C#/лекция1/Lecture1";
+DirectoryInfo di = new DirectoryInfo(path);
+
+WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    WriteLine(fi[i].Name);
+}
+WriteLine("rekurs search started");
+CatalogInfo(path);
+
+
+void CatalogInfo(string path, string ident = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for(int i=0; i< catalogs.Length; i++)
+    {
+        WriteLine($"{ident}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, ident + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+    for(int i=0; i< files.Length; i++)
+    {
+        WriteLine($"{ident}{files[i].Name}");
+    }
+
+}
