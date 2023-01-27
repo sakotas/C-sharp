a17560f8fd8fcc8a02a96d5be8720b56dfb9a9b2
Maksim_Dl <maksimdolgov@mail.ru>
2023-01-27 15:41:01 +0700

    add tasks for seminar 8

---
+++ Seminar8/Sem8_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+
+// 8 4 2 4
+
+// 5 9 2 3
+
+// 1 4 7 2
+
+using static System.Console;
+Clear();
+
+int[,] array = MyLibArray.IntMultiDimentionalArray.GenerateIntMNArray(3,4,0,10);
+MyLibArray.IntMultiDimentionalArray.PrintIntMNArray(array);
+
+
+int[] tempArr = new int[array.GetLength(1)];
+
+for(int i = 0; i < array.GetLength(1); i++)
+{
+    tempArr[i] = array[0,i];
+    array[0,i] = array[array.GetLength(0)-1, i];
+    array[array.GetLength(0)-1, i] = tempArr[i];
+
+}
+WriteLine();
+MyLibArray.IntMultiDimentionalArray.PrintIntMNArray(array);
+
+
---
+++ Seminar8/Sem8_4/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 59: Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+
+using static System.Console;
+Clear();
+
+int[,] array = MyLibArray.IntMultiDimentionalArray.GenerateIntMNArray(4,4,1,10);
+MyLibArray.IntMultiDimentionalArray.PrintIntMNArray(array);
+
+int minValue = array[0,0];
+int minIndexI = 0;
+int minIndexJ = 0;
+
+for( int i =0; i < array.GetLength(0); i++)
+{
+    for( int j =0; j < array.GetLength(1); j++)
+    {
+        if (array[i,j] < minValue)
+        {
+            minValue = array[i,j];
+            minIndexI = i;
+            minIndexJ = j;
+        }
+    }
+}
+WriteLine($"min element = {minValue} [{minIndexI},{minIndexJ}]");
+
+ int[,] AfterDelArray = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+ AfterDelArray = DelRowCol(array, minIndexI, minIndexJ);
+ MyLibArray.IntMultiDimentionalArray.PrintIntMNArray(AfterDelArray);
+
+
+/////////////methods below /////////
+
+int[,] DelRowCol(int[,] inArray, int iRow, int jCol)
+{
+    int[,] result = new int[inArray.GetLength(0)-1, inArray.GetLength(1)-1];
+
+int AfterDelI = 0;
+int AfterDelJ = 0;
+
+WriteLine ($"We dell Row= {iRow}, Col = {jCol}");
+
+    for (int i =0; i < inArray.GetLength(0)-1; i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1)-1; j++)
+        {
+            if (j < jCol)
+            {
+                AfterDelJ  = j;
+            }
+            else
+            {
+                AfterDelJ = j+1;
+            }
+            
+            if (i < iRow)
+            {
+                AfterDelI  = i;
+            }
+            else
+            {
+                AfterDelI = i+1;
+            }
+
+            result[i,j] = array[AfterDelI,AfterDelJ];
+        }
+    }
+
+return result;   
+}
